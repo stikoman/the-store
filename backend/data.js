e17efe15229f2eb0faddd0dc18e15f96306a0c6c
Steven Kwasiba <stikowebdevelopment@gmail.com>
2022-04-15 19:49:20 -0300

    Video-19-Create-Sample-Products-In-MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs';
+
 const data = {
   users: [
     {
       name: 'Kwasiba',
       email: 'admin@example.com',
-      password: bcrypt.hashSync('12345', 8),
+      password: bcrypt.hashSync('1234', 8),
       isAdmin: true,
     },
     {
       name: 'John',
       email: 'user@example.com',
-      password: bcrypt.hashSync('12345', 8),
+      password: bcrypt.hashSync('1234', 8),
       isAdmin: false,
     },
   ],
   products: [
     {
-      _id: '1',
       name: 'Nike Slim Shirt',
       category: 'Shirts',
       image: '/images/p1.jpg',
@@ -28,7 +28,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '2',
       name: 'Adidas Fit Shirt',
       category: 'Shirts',
       image: '/images/p2.jpg',
@@ -40,7 +39,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
       name: 'Lacoste Free Shirt',
       category: 'Shirts',
       image: '/images/p3.jpg',
@@ -52,7 +50,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
       name: 'Nike Slim Pant',
       category: 'Pants',
       image: '/images/p4.jpg',
@@ -64,7 +61,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '5',
       name: 'Puma Slim Pant',
       category: 'Pants',
       image: '/images/p5.jpg',
@@ -76,7 +72,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '6',
       name: 'Adidas Fit Pant',
       category: 'Pants',
       image: '/images/p6.jpg',
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/kws', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
+mongoose.connect(
+  process.env.MONGODB_URL || 'mongodb://localhost/the-kws-store',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   }
-});
+);
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
